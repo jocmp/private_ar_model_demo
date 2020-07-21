6d718dbf2b089c3abe71c026ca7721ca681f3810
Josiah Campbell <jocmp64@gmail.com>
2020-07-21 16:03:56 -0500

    Add comments

--- app/models/accounts.rb
+++ app/models/accounts.rb
@@ -1,5 +1,4 @@
 module Accounts
-
   # --- Public APIs
   def self.fetch(id:)
     db_object = Model.find(id)
@@ -18,12 +17,14 @@ module Accounts
   end
 
   # --- Private ActiveRecord model
+  # ---- No validations should exist at this level!
   class Model < ApplicationRecord
     self.table_name = 'accounts'
   end
   private_constant :Model
 
   # --- Value Object for the outside world
+  # ---- This can live anywhere, even outside of the codebase
   class Account
     attr_reader :id, :name
 
